157975a7132dc08f7993e2a0cfa5cec6070a1cbd
Daniel Tinkov <dtinkov@gmail.com>
2013-09-13 19:57:43 +0300

    added video feed with png

--- javascript/main.js
+++ javascript/main.js
@@ -1,11 +1,18 @@
 var $ = require("jquery");
 var arDrone = require("ar-drone");
 var client = arDrone.createClient();
-var videoStream = client.getVideoStream();
+var pngStream = client.getPngStream();
 var flightState = false;
 
-client.on("navdata", console.log);
-videoStream.on("data", console.log);
+client.on("navdata", function(data) {
+    console.log(data);
+});
+pngStream.on("data", function (frame) {
+    var base64Image = new Buffer(frame, "binary").toString("base64");
+    requestAnimationFrame(function() {
+        $("img").attr("src", 'data:image/png;base64,' + base64Image);
+    });
+});
 
 $(function () {
     $("a").on("click", function () {
